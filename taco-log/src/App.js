d50f5aab81779979dc73ec9247b6d379c0f8a84f
totsuya <supertatortots@gmail.com>
2019-02-13 16:17:51 -0500

    set things up for react strap later down the line

--- taco-log/src/App.js
+++ taco-log/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Map from './Components/map.js'
+import 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {/* <Map /> */}
+        <p>Taco Log!</p>
+        
         </header>
-
       </div>
     );
   }
