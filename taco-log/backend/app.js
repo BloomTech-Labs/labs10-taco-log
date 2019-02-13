1357e13a93950655caded1e483be88cb8ded6df2
farste <sfargali@gmail.com>
2019-02-13 12:49:10 -0500

    Made preparations to build db, installed knex, cors, express, sqlite3

--- taco-log/backend/app.js
+++ taco-log/backend/app.js
@@ -1,3 +1,20 @@
+/*
+const express = require("express");
+const knex = require("knex");
+const server = express();
+const dbConfig = require("./knexfile.js");
+const db = knex(dbConfig.development);
+const cors = require("cors");
+server.use(cors());
+server.use(express.json());
+
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
