236f432d502ce156be3f9b37e0e78363912a4d4c
farste <sfargali@gmail.com>
2019-02-13 18:46:15 -0500

    revised app.js

--- taco-log/backend/app.js
+++ taco-log/backend/app.js
@@ -1,25 +1,14 @@
 
-
 //const express = require("express");
 //const knex = require("knex");
-//const server = express();
+const server = express();
+const http = require ("http");
 //const dbConfig = require("../knexfile.js");
 //const db = knex(dbConfig.development);
 //const cors = require("cors");
 //server.use(cors());
 //server.use(express.json());
 
-const http = require('http');
-
-const hostname = "taco-log-backend.herokuapp.com";
-const port = process.env.PORT || 5000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.get("/", (req, res) => {
+  res.json({message: "Hello, World"});
+})
