1ac03aa89d948ceab1b4b550388b92fab987abc2
Brandon White <bwhite06@stac.edu>
2019-02-13 22:53:49 -0500

    added to db; created db user helpers
    --working on seeding

--- taco-log/backend/app.js
+++ taco-log/backend/app.js
@@ -8,9 +8,22 @@ const http = require ("http");
 //const cors = require("cors");
 //server.use(cors());
 //server.use(express.json());
+const userDb = require('./database/helpers/dbhelper.js');
+
 
 server.get("/", (req, res) => {
   res.json({message: "Hello, World"});
 })
+
+server.get('/api/users',(req,res)=>{
+  userDb.get()
+  .then(user =>{
+    console.log('Success', user);
+    res.status(200).json(user)
+  })
+  .catch(err =>{
+    res.send(err)
+  } )
+});
 server.listen(process.env.PORT, () =>
   {console.log("test")});
