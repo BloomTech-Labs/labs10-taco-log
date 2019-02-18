1d7937ed5ac4b17054a8a67cd8891bb3e86ba217
totsuya <supertatortots@gmail.com>
2019-02-18 18:00:40 -0500

    added some comments regarding API key

--- taco-log/src/Components/map.js
+++ taco-log/src/Components/map.js
@@ -14,10 +14,13 @@ class Map extends Component {
     this.props.onMapLoad(map)
   }
 
+  //allows the React application to load in first to ensure that the google maps API works fine
+
   componentDidMount() {
     if (!window.google) {
       const script = document.createElement('script');
       script.type = 'text/javascript';
+      //note: this part here is the google API key, please replace as needed
       script.src = `https://maps.google.com/maps/api/js?key=AIzaSyDUwSdMdxb22hcjJE3eJ6qycHz8aofkY0k`;
       const tagname = document.getElementsByTagName('script')[0];
       tagname.parentNode.insertBefore(script, tagname);
