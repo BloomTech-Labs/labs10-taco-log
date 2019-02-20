36fdc5a29dc2edc00e3716034873469d06650ac9
jaymzsocool <james.mcraejr@gmail.com>
2019-02-20 12:41:39 -0500

    changed to heroku url for test with heroku

--- taco-log/src/Components/LoginPage.js
+++ taco-log/src/Components/LoginPage.js
@@ -4,6 +4,9 @@ import { Button } from 'reactstrap';
 import { firebase, provider } from '../firebase/firebase';
 import axios from 'axios'
 
+const local = 'http://localhost:5000/'
+const heroku = 'https://tacobe.herokuapp.com/'
+
 class LoginPage extends Component {
     constructor() {
         super();
@@ -24,12 +27,12 @@ class LoginPage extends Component {
                 ext_user_id: result.user.uid
             }                       
             axios
-                .get('http://localhost:5000/api/users') 
+                .get(`${heroku}api/users`) 
                 .then(res => {                    
                     let post = true
                     for(let i = 0; i < res.data.length; i++){
                         if(res.data[i].ext_user_id == user.ext_user_id){
-                           axios.get(`http://localhost:5000/api/users/${res.data[i].internal_id}`)
+                           axios.get(`${heroku}api/users/${res.data[i].internal_id}`)
                            .then(res =>{                                
                                 this.setState({userInfo: res.data})
                            })
@@ -41,10 +44,10 @@ class LoginPage extends Component {
                     }       
                     if(post){
                         axios    
-                            .post('http://localhost:5000/api/users', user)
+                            .post(`${heroku}api/users`, user)
                             .then(res => {   
                                 const stats = { user_id: res.data.internal_id}    
-                                axios.post('http://localhost:5000/api/user_stats', stats)
+                                axios.post(`${heroku}api/user_stats`, stats)
                                 .then(res => {
                                     this.setState({userInfo: res.data})
                                 })  
