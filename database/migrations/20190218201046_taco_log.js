60ff08532f860e5017b3df42313e83df3feeb0af
farste <sfargali@gmail.com>
2019-02-25 18:53:41 -0500

    Basic updates to logtaco to connect to maps api, changes to migration

--- database/migrations/20190218201046_taco_log.js
+++ database/migrations/20190218201046_taco_log.js
@@ -1,21 +1,21 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('taco-log', function(tbl) {
-  tbl.increments("id");
-  tbl
-    .integer('user_id')
-    .unsigned()
-    .notNullable()
-    .references('internal_id')
-    .inTable('users') // id,
-  tbl
-    .string('taco_location')    
-  tbl
-    .string('taco_description')
-  tbl  
-    .decimal('rating',0,5)
-  tbl
-    .timestamp('created_at').defaultTo(knex.fn.now()); 
-  })
+  return knex.schema.createTable("taco-log", function(tbl) {
+    tbl.increments("id");
+    tbl
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("internal_id")
+      .inTable("users"); // id,
+    tbl.string("taco_location");
+    tbl.string("location_id");
+    tbl.string("taco_coordinates");
+    tbl.string("ingredients").notNullable();
+    tbl.integer("special_experience").defaultTo(0);
+    tbl.string("taco_description");
+    tbl.decimal("rating", 0, 5);
+    tbl.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex, Promise) {
