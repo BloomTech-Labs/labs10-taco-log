d108d475e12e0b49a4af0d46b4bb1812f0565397
totsuya <supertatortots@gmail.com>
2019-02-27 15:12:57 -0500

    set accountsettings page

---
+++ taco-log/src/Components/AccountSettings.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { InputGroup, Input }  from 'reactstrap';
+class AccountSettings extends Component {
+  render() {
+    <div>
+      Adjust your display name here!
+      <InputGroup>
+      <Input placeholder="DisplayName" />
+      </InputGroup>
+    </div>
+  }
+}
+
+export default AccountSettings;
