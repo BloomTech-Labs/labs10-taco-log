9eb2312159c0de56f6de796fa4427877846c9514
totsuya <supertatortots@gmail.com>
2019-02-28 12:54:35 -0500

    added submit

--- taco-log/src/Components/AccountSettings.js
+++ taco-log/src/Components/AccountSettings.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import { InputGroup, Input }  from 'reactstrap';
+import { InputGroup, Input, Button }  from 'reactstrap';
 class AccountSettings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ''
+    }
+  this.onUsernameChange = this.onUsernameChange.bind(this);
+  }
+
+  onUsernameChange(e) {
+    this.setState({ username: event.target.value });
+  }
+
+  handleSubmit(e) {
+    event.preventDefault();
+    sessionStorage.setItem('username', this.username);
+  }
+
   render() {
     return (
     <div>
       Adjust your display name here!
       <InputGroup>
-      <Input placeholder="DisplayName" />
+      <Input type="text" value={this.username} onChange{...this.onUsernameChange} placeholder="DisplayName" />
+      <Button color="Primary">Save username</Button>
       </InputGroup>
     </div>
     )
