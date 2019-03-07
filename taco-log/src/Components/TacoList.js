fa3496871ac057ea0cc33982166cf37c2dacf048
farste <39572677+farste@users.noreply.github.com>
2019-03-07 15:11:02 -0500

    Update TacoList.js

--- taco-log/src/Components/TacoList.js
+++ taco-log/src/Components/TacoList.js
@@ -1,12 +1,5 @@
 import React, { Component } from "react";
 import TacoCard from "./TacoCard";
-import "../css/TacoList.css"
-
-class TacoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {import React, { Component } from "react";
-import TacoCard from "./TacoCard";
 import "../css/TacoList.css";
 
 class TacoList extends Component {
@@ -115,106 +108,6 @@ class TacoList extends Component {
             </div>
           </div>
         )}
-      </div>
-    );
-  }
-}
-
-export default TacoList;
-
-      cities: [],
-      cityfilter: "All",
-      filteredLogs: []
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  componentDidMount = () => {    
-    let cities = [];
-    for (let i = 0; i < this.props.userInfo.taco_logs.length; i++) {
-      let city = this.props.userInfo.taco_logs[i].address.split(",")[1].trim();
-      if(cities.indexOf(city) === -1){
-        cities.push(city);
-      }
-      
-    }
-    this.setState({
-      cities: cities
-    });
-  };
-
-  // componentDidUpdate(prevState) {
-  //   if (
-  //     prevState.cityfilter &&
-  //     prevState.cityfilter !== this.state.cityfilter
-  //   ) {
-  //   }
-  // }
-
-  handleChange(e) {
-    e.preventDefault();
-    
-    const filteredLogs = this.props.userInfo.taco_logs.filter(log => { 
-      return log.address.split(",")[1].trim() === e.target.value;
-    });
-    this.setState({ filteredLogs: filteredLogs, cityfilter: e.target.value });
-  }
-
-  render() {
-    console.log(this.state);
-    return (
-      <div>
-        <div>
-          Filter by City:
-          <select value={this.state.cityfilter} onChange={this.handleChange}>
-            <option value="All">All</option>
-            {this.state.cities.map(city => (
-              <option value={city}>{city}</option>
-            ))}
-          </select>
-        </div>
-        <div className="taco-list-owrap">
-          
-          {this.state.cityfilter === "All" ? (
-            <div className="taco-list-iwrap">
-              {this.props.userInfo.taco_logs.map(log => (
-                <TacoCard
-                  {...this.props}
-                  key={log.id}
-                  id={log.id}
-                  taco_location={log.taco_location}
-                  taco_description={log.taco_description}
-                  rating={log.rating}
-                  tortilla={log.tortilla}
-                  meat={log.meat}
-                  cheese={log.cheese}
-                  salsa={log.salsa}
-                  created_at={log.created_at}
-                  address = {log.address}
-                />
-              ))}
-            </div>
-          ) : (
-            <div className="taco-list-iwrap">
-              {this.state.filteredLogs.map(log => (
-                <TacoCard
-                {...this.props}
-                key={log.id}
-                id={log.id}
-                taco_location={log.taco_location}            
-                taco_description={log.taco_description}
-                rating={log.rating}
-                tortilla={log.tortilla}
-                meat={log.meat}
-                cheese={log.cheese}
-                salsa={log.salsa}
-                created_at={log.created_at}
-                address = {log.address}
-              />
-              ))}
-            </div>
-          )}
-        </div>
       </div>
     );
   }
