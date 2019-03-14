94b87ff29e5b3ce27456a2a03404b1c4d1aa7cac
jaymzsocool <james.mcraejr@gmail.com>
2019-03-13 20:42:31 -0400

    search bar on landing page functional. info is stored and loaded when routed to taco log after log in.

--- taco-log/src/actions/tacoActions.js
+++ taco-log/src/actions/tacoActions.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 export const FETCH_TACOS = 'FETCH_TACOS';
 export const ADDDATABASE_TACOS = 'ADDDATABASE_TACOS';
+export const STORE_LANDING_TACO = 'STORE_LANDING_TACO';
+export const CLEAR_LANDING_TACO = 'CLEAR_LANDING_TACO';
 
 const local = 'http://localhost:5000/';
 const heroku = 'https://tacobe.herokuapp.com/';
@@ -17,3 +19,9 @@ export const ADD_TACO = taco => dispatch => {
     .post(`${link}api/friends`, taco)
     .then(res => dispatch({ type: ADDDATABASE_TACOS, payload: res.data }));
 };
+export const storeLandingTaco = taco => dispatch =>{
+  dispatch({ type: STORE_LANDING_TACO, payload: taco })
+}
+export const clearLandingTaco = taco => dispatch =>{
+  dispatch({ type: CLEAR_LANDING_TACO })
+}
--- taco-log/src/reducers/tacoReducer.js
+++ taco-log/src/reducers/tacoReducer.js
@@ -1,7 +1,8 @@
-import { FETCH_TACOS, ADDDATABASETACOS } from '../actions';
+import { FETCH_TACOS, ADDDATABASETACOS, STORE_LANDING_TACO, CLEAR_LANDING_TACO } from '../actions';
 
 const initialState = {
-  tacos: []
+  tacos: [],
+  landingTaco: []
 };
 
 export default (state = initialState, action) => {
@@ -11,6 +12,16 @@ export default (state = initialState, action) => {
         ...state,
         tacos: action.payload
       };
+    case STORE_LANDING_TACO:
+      return {
+        ...state,
+        landingTaco: action.payload
+      }
+    case CLEAR_LANDING_TACO:
+      return {
+        ...state,
+        landingTaco: []
+      }
     default:
       return state;
   }
